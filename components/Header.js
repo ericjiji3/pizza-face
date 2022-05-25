08845e610296bfdd3cb9ad38e51b6dd2d44c26dd
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-05-24 20:05:43 -0500

    mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -4,41 +4,154 @@ import Image from "next/image";
 import discord from "../public/images/header/discord.png";
 import twitter from "../public/images/header/twitter.png";
 import boat from "../public/images/header/boat.png";
+import { React, useState } from 'react'
+import { useCallback, useEffect } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+
+var styles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      left: '36px',
+      top: '36px'
+    },
+    bmBurgerBars: {
+      background: '#373a47'
+    },
+    bmBurgerBarsHover: {
+      background: '#a90000'
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px'
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      height: '100%'
+    },
+    bmMenu: {
+      background: '#373a47',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em'
+    },
+    bmItem: {
+      display: 'inline-block'
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
+  }
+
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+  
+    const updateTarget = useCallback((e) => {
+      if (e.matches) {
+        setTargetReached(true);
+      } else {
+        setTargetReached(false);
+      }
+    }, []);
+  
+    useEffect(() => {
+      const media = window.matchMedia(`(max-width: ${width}px)`);
+      media.addListener(updateTarget);
+  
+      // Check on mount (callback is not called until a change occurs)
+      if (media.matches) {
+        setTargetReached(true);
+      }
+  
+      return () => media.removeListener(updateTarget);
+    }, []);
+  
+    return targetReached;
+  };
 
 export default function Header() {
+    const isBreakpoint = useMediaQuery(700);
+    const showSettings = (event) => {
+        event.preventDefault();
+      }
     return (
-        <div className="h-container">
-            <h1>PIZZA BOY</h1>
-            <div className="h-links">
-                <ul>
-                    <li>
-                    <Link to="about" spy={true} smooth={true} duration={500}>
-                            ABOUT
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="roadmap" spy={true} smooth={true} duration={500}>
-                        ROADMAP
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to="team" spy={true} smooth={true} duration={500}>
-                        TEAM
-                    </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="socials">
-                <a href="#">
-                    <Image src={discord} />
-                </a>
-                <a href="#">
-                    <Image src={twitter} />
-                </a>
-                <a href="#">
-                    <Image src={boat} />
-                </a>
-            </div>
+        <div>        
+            {isBreakpoint ? (
+                <div className="h-container">
+                    <h1>PIZZA BOY</h1>
+                    <Menu styles={ styles } right>
+                        <ul>
+                            <li>
+                            <Link to="about" spy={true} smooth={true} duration={500} offset={-50}>
+                                    ABOUT
+                            </Link>
+                            </li>
+                            <li>
+                            <Link to="roadmap" spy={true} smooth={true} duration={500} offset={-50}>
+                                ROADMAP
+                            </Link>
+                            </li>
+                            <li>
+                            <Link to="team" spy={true} smooth={true} duration={500} offset={-50}>
+                                TEAM
+                            </Link>
+                            </li>
+                        </ul>
+                    </Menu>
+                </div>
+            ) : (
+                <div className="h-container">
+                    <h1>PIZZA BOY</h1>
+                    <div className="h-links">
+                        <ul>
+                            <li>
+                            <Link to="about" spy={true} smooth={true} duration={500}>
+                                    ABOUT
+                            </Link>
+                            </li>
+                            <li>
+                            <Link to="roadmap" spy={true} smooth={true} duration={500}>
+                                ROADMAP
+                            </Link>
+                            </li>
+                            <li>
+                            <Link to="team" spy={true} smooth={true} duration={500}>
+                                TEAM
+                            </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="socials">
+                        <a href="#">
+                            <div className="image-wrapper">
+                                <Image src={discord} />
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className="image-wrapper">
+                                <Image src={twitter} />
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className="image-wrapper">
+                                <Image src={boat} />
+                            </div>
+                        </a>
+                    </div>
+                    </div>
+            )}
+            
       </div>
     );
   }
--- pages/RoadMap.js
+++ pages/RoadMap.js
@@ -3,12 +3,48 @@ import percentagePic from "../public/images/roadmap/percentage-pic.png";
 import plusButton from "../public/images/roadmap/plus-button.png";
 import minusButton from "../public/images/roadmap/minus-button.png";
 import pizzaFace from "../public/images/roadmap/pizza-face.png";
+import { React, useState } from 'react'
+import { useCallback, useEffect } from 'react';
 
-
-
-
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+  
+    const updateTarget = useCallback((e) => {
+      if (e.matches) {
+        setTargetReached(true);
+      } else {
+        setTargetReached(false);
+      }
+    }, []);
+  
+    useEffect(() => {
+      const media = window.matchMedia(`(max-width: ${width}px)`);
+      media.addListener(updateTarget);
+  
+      // Check on mount (callback is not called until a change occurs)
+      if (media.matches) {
+        setTargetReached(true);
+      }
+  
+      return () => media.removeListener(updateTarget);
+    }, []);
+  
+    return targetReached;
+  };
 
 export default function RoadMap() {
+    const [counter, setCounter] = useState(0);
+    const isBreakpoint = useMediaQuery(700);
+
+    const increase = () =>{
+        setCounter(count => count + 1);
+    };
+
+    const decrease = () =>{
+        setCounter(count => count - 1)
+    };
+
+
     return (
       <div id="roadmap" className="r-container">
         <h1>ROADMAP</h1>
@@ -23,6 +59,37 @@ export default function RoadMap() {
                         2.5ETH FOR COMMUNITY WALLET.
                     </span>
                 </div>
+                {isBreakpoint ? (
+                <div className="percentages">
+                        <h1>50%</h1>
+                        <span>
+                            RELEASE STATS ON RARITY TOOL.
+                        </span>
+                        <span>
+                            2.5ETH TO SLICE OUT HUNGER.
+                        </span>
+                        <span>
+                            2.5ETH FOR COMMUNITY WALLET.
+                        </span>
+                </div>
+                ) : (
+                <div className="percentages">
+                        <h1>80%</h1>
+                        <span>
+                            LAUNCH MERCHANDISE WEBSITE.
+                        </span>
+                        <span>
+                            ALL PROFITS ARE DONATED TO SLICE OUT HUNGER.
+                        </span>
+                </div>
+                )
+                }
+            </div>
+            <div className="image-wrapper">
+                <Image src={percentagePic}/>
+            </div>
+            <div className="goal-container">
+                {isBreakpoint ? (
                 <div className="percentages">
                     <h1>80%</h1>
                     <span>
@@ -32,11 +99,7 @@ export default function RoadMap() {
                         ALL PROFITS ARE DONATED TO SLICE OUT HUNGER.
                     </span>
                 </div>
-            </div>
-            <div className="image-wrapper">
-                <Image src={percentagePic}/>
-            </div>
-            <div className="goal-container">
+                ):(
                 <div className="percentages">
                     <h1>50%</h1>
                     <span>
@@ -49,6 +112,8 @@ export default function RoadMap() {
                         2.5ETH FOR COMMUNITY WALLET.
                     </span>
                 </div>
+                )
+                }
                 <div className="percentages">
                     <h1>100%</h1>
                     <span>
@@ -69,11 +134,11 @@ export default function RoadMap() {
                 <h2>PLEASE CONNECT YOUR WALLET.</h2>
                 <div className="inventory">
                     <div className="image-wrapper">
-                        <Image src={plusButton}/>
+                        <Image src={plusButton} onClick={increase}/>
                     </div>
-                    <span className="amount">1</span>
+                    <span className="amount">{counter}</span>
                     <div className="image-wrapper">
-                        <Image src={minusButton}/>
+                        <Image src={minusButton} onClick={decrease}/>
                     </div>
                 </div>
                 <div className="buy">
